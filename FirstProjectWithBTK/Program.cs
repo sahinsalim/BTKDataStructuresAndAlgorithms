2333e01a90ba4c31b9544f6e72c7dd601daddc08
Salim Sahin <salimshn02@gmail.co>
2024-01-30 20:29:12 +0300

    Add project files.

---
+++ FirstProjectWithBTK/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+
+        Console.ReadKey();
+    }
+
+    private static void DegiskenOrnekleri()
+    {
+        // TYPES
+
+        //int a = 1;
+        //int b = 2 * a;
+        //int sayi1;
+        //double pi = 3.14;
+        //char secim = 'A';
+        //string isim = "Salim";
+        //int a = 91;
+        //var c = 20 * a;
+        //var b = a + c;
+        //int sayi = 5;
+
+        string Name = "Salim";
+        string Surname = "Sahin";
+        int Age = 21;
+        string University = "Samsun University";
+        int Class = 4;
+
+        Console.WriteLine("Hi, I'm " + Name + " " + Surname + " And I am "
+            + Age + " and I'm studying as a " + Class + ". year student at " + University + ".");
+
+
+        //Console.WriteLine(sayi);
+        //Console.WriteLine(sayi*2);
+        //Console.WriteLine(sayi+2);
+        //Console.WriteLine(sayi-2);
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
+        //Console.WriteLine(c);
+        //Console.WriteLine(a*b);
+        //Console.WriteLine(c+a);
+
+
+
+        //string isim = "Ahmet";
+        //Console.WriteLine("Merhaba " + isim + ".");
+        //isim = "Salim";
+        //Console.WriteLine("Merhaba Sayın " + isim + ".");
+
+        Console.ReadKey();
+    }
+}
